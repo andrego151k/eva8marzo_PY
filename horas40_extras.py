60f95a7da91d1b008a7f45b163ae9320f79e9131
andres gomez <161092979+andrego151k@users.noreply.github.com>
2024-03-09 03:02:11 -0500

    Update horas40_extras.py
    cambio en nombres

--- horas40_extras.py
+++ horas40_extras.py
@@ -1,11 +1,11 @@
-Horas = int(input("ingrese total de horas trabajadas: "))
-Horasext = int(input("ingrese horas extras trabajadas: "))
+Hr = int(input("ingrese total de horas trabajadas: "))
+Horasext = int(input("ingrese horas extras trabajadas si tiene: "))
 Valor_hora = 10
-Valor_hextra = 5
+bono_hextra = 5
 
-if Horas <= 40:
-        pagohoras = Valor_hora * Horas
-        print (pagohoras)
-elif Horas > 40:
-            pagoextras = (Valor_hora * Horas) + (Horasext * Valor_hextra)
-            print (pagoextras)
+if Hr <= 40:
+    pagohoras = Valor_hora * Hr
+    print(pagohoras)
+elif Hr > 40:
+    pagoextras = (Valor_hora * Hr) + (Horasext * bono_hextra)
+    print(pagoextras)
